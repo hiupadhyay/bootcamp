c5e882c5e7735fd252e30dfccf01749831fc5128
Himanshu Upadhyay <upadhyay.himansh@gmail.com>
2021-11-25 15:17:37 +0000

    cleaned up changes

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from geopy.geocoders import Nominatim
 import geocoder
 import yfinance as yf
 
-
 app = Flask('bootcamp')
 
 # NOTE: EVERYTHING in this file is publically viewable. If you want to use private information (like private keys/passwords) in your application, please put them in a new .env file, and reference them like shown below.
@@ -15,12 +14,11 @@ app = Flask('bootcamp')
 # print(os.getenv("TWILIO_ACCOUNT_SID"))
 main_menu = """Welcome Amigos,Reply with Option  
   1) Joke
-  2) Current Weather
-  3) Stock Prize
-  4) What is my Address
-  5) Main Menu"""
+  2) Stock Prize
+  3) What is my Address
+  4) Main Menu"""
 
-stock_prize="Enter Ticker Symbol"
+stock_prize = "Enter Ticker Symbol"
 
 
 @app.route('/sms', methods=['GET', 'POST'])
@@ -30,21 +28,19 @@ def sms():
         return resp("Score is 250 +")
     elif choice == "hi" or choice == "hello" or choice == "tere" or choice == "namaste" or choice == "ola":
         return resp(main_menu)
-    elif choice == "4":
+    elif choice == "3":
         return resp(fetchLocation())
     elif choice == "1":
         return resp(fetchJoke())
-    elif choice == "3":
-        return resp(stock_prize)
     elif choice == "2":
-        return resp(fetchWeather())
-    elif choice == "5":
+        return resp(stock_prize)
+    elif choice == "4":
         return resp(main_menu)
     else:
-        if validTicker(choice)=="0":
-          return resp(stockprize(choice))
+        if validTicker(choice) == "0":
+            return resp(stockprize(choice))
         else:
-          return resp("""try again..
+            return resp("""try again..
         """ + main_menu + """
          """)
 
@@ -55,17 +51,19 @@ def resp(msg):
     <Message>""" + msg + """</Message>
   </Response>"""
 
+
 def validTicker(ticker):
-  try:
-    yf.Ticker(ticker)
-    return 0
-  except:
-    return 1
+    print(yf.Ticker(ticker))
+    try:
+        yf.Ticker(ticker)
+        return "0"
+    except:
+        return "1"
 
 
 def stockprize(ticker):
-   ticker = yf.Ticker(ticker).history(period='1d')
-   return str(ticker['Close'][0])
+    ticker = yf.Ticker(ticker).history(period='1d')
+    return str(ticker['Close'][0])
 
 
 def fetchLocation():
@@ -73,8 +71,7 @@ def fetchLocation():
     response = response.json()
     Latitude = response['iss_position']['latitude']
     Longitude = response['iss_position']['longitude']
-    return  str(fetchAddress(
-        Latitude, Longitude))
+    return str(fetchAddress(Latitude, Longitude))
 
 
 def fetchJoke():
@@ -95,12 +92,10 @@ def fetchAddress(latitude, longitude):
     print(location)
     address = location.raw['address']
     pairs = address.items()
-    str=""
+    str = ""
     for key, value in pairs:
-      str=str+ " "+value+ "\n"
+        str = str + " " + value + "\n"
     return str
-
-
 
 
 app.run(debug=True, host='0.0.0.0', port=8080)
